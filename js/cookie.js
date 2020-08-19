52cb48fc3a8977ba0e70d3a82f7e367599f804a7
Nicolas <ndepaula@gmail.com>
2020-08-19 20:17:04 -0300

    test

---
+++ js/cookie.js
@@ -0,0 +1,14 @@
+window.cookieconsent.initialise({
+    palette: {
+      popup: {
+        background: "#000",
+      },
+      button: {
+        background: "transparent",
+        text: "#f1d600",
+        border: "#f1d600",
+      },
+    },
+
+    return:
+  });
